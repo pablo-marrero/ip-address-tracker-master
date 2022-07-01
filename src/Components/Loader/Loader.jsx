74cd9ee314bce90e917ffbdbc9c01df7958a0286
pablo-marrero <pablomarrero241096@gmail.com>
2022-07-01 19:25:42 -0300

    works this change?

--- src/Components/Loader/Loader.jsx
+++ src/Components/Loader/Loader.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import "./Loader.css"
 
 export const Loader = () => {
+  //Funciona?
   return (
     <div className="container-loader">
     <div className="lds-spinner">
